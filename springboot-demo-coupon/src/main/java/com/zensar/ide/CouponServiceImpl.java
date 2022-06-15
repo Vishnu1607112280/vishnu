9555256fc7c70e09c563e8a3869661a0b786e8d8
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-15 22:54:08 +0530

    Update CouponServiceImpl.java

--- springboot-demo-coupon/src/main/java/com/zensar/ide/CouponServiceImpl.java
+++ springboot-demo-coupon/src/main/java/com/zensar/ide/CouponServiceImpl.java
@@ -1,10 +1,13 @@
 package com.zensar.ide;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zensar.springbootdemoCouponDto.CouponDto;
 import com.zensar.ide.entity.Coupon;
 import com.zensar.ide.repository.CouponRepository;
 
@@ -13,27 +16,44 @@ public class CouponServiceImpl implements CouponService {
 	@Autowired
 	private CouponRepository couponRepository;
 
+	@Autowired
+	private ModelMapper modelMapper;
+
 	public CouponServiceImpl() {
 	}
 
 	@Override
-	public Coupon getCoupon(int couponId) {
-		return couponRepository.findById(couponId).get();
-	}
-
-	@Override
-	public List<Coupon> getCoupons() {
-		return couponRepository.findAll();
-	}
-
-	@Override
-	public void insertCoupon(Coupon coupon) {
-		couponRepository.save(coupon);
+	public CouponDto getCoupon(int couponId) {
+		Coupon getCoupon = couponRepository.findById(couponId).get();
+		return modelMapper.map(getCoupon, CouponDto.class);
 
 	}
 
 	@Override
-	public void updateCoupon(int couponId, Coupon coupon) {
+	public List<CouponDto> getCoupons() {
+		List<Coupon> listOfCoupons = couponRepository.findAll();
+		List<CouponDto> listOfCouponDto = new ArrayList<CouponDto>();
+		for (Coupon coupon : listOfCoupons) {
+
+			listOfCouponDto.add(modelMapper.map(coupon, CouponDto.class));
+		}
+		return listOfCouponDto;
+	}
+
+	@Override
+	public CouponDto insertCoupon(CouponDto couponDto) {
+
+		Coupon coupon = modelMapper.map(couponDto, Coupon.class);
+		Coupon insertedCoupon = couponRepository.save(coupon);
+
+		return modelMapper.map(insertedCoupon, CouponDto.class);
+
+	}
+
+	@Override
+	public void updateCoupon(int couponId, CouponDto couponDto) {
+
+		Coupon coupon = modelMapper.map(couponDto, Coupon.class);
 		couponRepository.save(coupon);
 
 	}
