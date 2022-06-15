3cb3ee364e8d2318c6f9fe013c25bbc675686dcf
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-15 22:50:24 +0530

    Update Coupon.java

--- springboot-demo-coupon/src/main/java/com/zensar/ide/entity/Coupon.java
+++ springboot-demo-coupon/src/main/java/com/zensar/ide/entity/Coupon.java
@@ -6,59 +6,48 @@ import javax.persistence.Id;
 @Entity
 public class Coupon {
 	@Id
-	private int couponId;
-	private String couponName;
-	private int couponPrice;
-	private String couponDesc;
+	private int CouponId;
+	private String CouponCode;
+	private String CouponExpdate;
+
+	public int getCouponId() {
+		return CouponId;
+	}
+
+	public void setCouponId(int couponId) {
+		CouponId = couponId;
+	}
+
+	public String getCouponCode() {
+		return CouponCode;
+	}
+
+	public void setCouponCode(String couponCode) {
+		CouponCode = couponCode;
+	}
+
+	public String getCouponExpdate() {
+		return CouponExpdate;
+	}
+
+	public void setCouponExpdate(String couponExpdate) {
+		CouponExpdate = couponExpdate;
+	}
 
 	public Coupon() {
 		super();
 	}
 
-	public Coupon(int couponId, String couponName, int couponPrice, String couponDesc) {
+	public Coupon(int couponId, String couponCode, String couponExpdate) {
 		super();
-		this.couponId = couponId;
-		this.couponName = couponName;
-		this.couponPrice = couponPrice;
-		this.couponDesc = couponDesc;
-	}
-
-	public int getCouponId() {
-		return couponId;
-	}
-
-	public void setCouponId(int couponId) {
-		this.couponId = couponId;
-	}
-
-	public String getCouponName() {
-		return couponName;
-	}
-
-	public void setCouponName(String couponName) {
-		this.couponName = couponName;
-	}
-
-	public int getCouponPrice() {
-		return couponPrice;
-	}
-
-	public void setCouponPrice(int couponPrice) {
-		this.couponPrice = couponPrice;
-	}
-
-	public String getCouponDesc() {
-		return couponDesc;
-	}
-
-	public void setCouponDesc(String couponDesc) {
-		this.couponDesc = couponDesc;
+		CouponId = couponId;
+		CouponCode = couponCode;
+		CouponExpdate = couponExpdate;
 	}
 
 	@Override
 	public String toString() {
-		return "Coupon [couponId=" + couponId + ", couponName=" + couponName + ", couponPrice=" + couponPrice
-				+ ", couponDesc=" + couponDesc + "]";
+		return "Coupon [CouponId=" + CouponId + ", CouponCode=" + CouponCode + ", CouponExpdate=" + CouponExpdate + "]";
 	}
 
 }
