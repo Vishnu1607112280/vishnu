a65fd377901fb26f142d9d88cf55b03496249001
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-16 23:06:18 +0530

    Update CouponService.java

--- springboot-demo-coupon/src/main/java/com/zensar/ide/CouponService.java
+++ springboot-demo-coupon/src/main/java/com/zensar/ide/CouponService.java
@@ -8,11 +8,21 @@ import com.zensar.ide.entity.Coupon;
 public interface CouponService {
 	public CouponDto getCoupon(int CouponId);
 
-	public List<CouponDto> getCoupons();
+	// public List<CouponDto> getCoupons();
+	public List<CouponDto> getCoupons(int pageNumber, int pageSize);
 
 	public CouponDto insertCoupon(CouponDto couponDto);
 
 	public void updateCoupon(int CouponId, CouponDto couponDto);
 
 	public void deleteCoupon(int CouponId);
+
+	public List<CouponDto> getByCouponCode(String couponCode);
+
+	public List<CouponDto> getByCouponCodeOrCouponPrice(String couponCode, int price);
+
+	public List<CouponDto> getByCouponCodeAndExpDate(String couponCode, String expDate);
+
+	public List<CouponDto> getByCouponCodeOrderByCouponPrice(String couponCode);
+
 }
