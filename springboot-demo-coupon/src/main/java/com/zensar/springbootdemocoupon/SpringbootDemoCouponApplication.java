4918c70dc7aa5e83b0f10ca58e2bcbf777314073
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-23 08:24:17 +0530

    Update SpringbootDemoCouponApplication.java

--- springboot-demo-coupon/src/main/java/com/zensar/springbootdemocoupon/SpringbootDemoCouponApplication.java
+++ springboot-demo-coupon/src/main/java/com/zensar/springbootdemocoupon/SpringbootDemoCouponApplication.java
@@ -1,13 +1,31 @@
-package com.zensar.springbootdemocoupon;
-
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
-public class SpringbootDemoCouponApplication {
+@OpenAPIDefinition
+public class SpringBootCouponServiceApplication {
+	/*
+	 * public class SpringBootCouponServiceApplication extends
+	 * SpringBootServletInitializer{
+	 * 
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder builder) {
+	 * 
+	 * return super.configure(builder); }
+	 */
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoCouponApplication.class, args);
+		SpringApplication.run(SpringBootCouponServiceApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 }
