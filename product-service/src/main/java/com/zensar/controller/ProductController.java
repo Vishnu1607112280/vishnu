7c875162278a1f8749bc804285edf942738ba8a5
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-26 06:40:44 +0530

    Update ProductController.java

--- product-service/src/main/java/com/zensar/controller/ProductController.java
+++ product-service/src/main/java/com/zensar/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.zensar.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import com.zensar.entity.Coupon;
 import com.zensar.entity.CouponClassList;
 import com.zensar.entity.Product;
+import com.zensar.springbootdemo.RestClient.CouponRestClient;
 import com.zensar.services.ProductService;
 
 @RestController
@@ -24,22 +26,34 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
+	/*
+	 * @Autowired private RestTemplate restTemplate;
+	 */
+
 	@Autowired
-	private RestTemplate restTemplate;
+	private CouponRestClient restClient;
 
 	@PostMapping("/")
 	public Product insertProduct(@RequestBody Product product) {
 
-		ResponseEntity<Coupon> coupon = restTemplate
-				.getForEntity("http://localhost:1234/coupons/" + product.getCouponCode(), Coupon.class);
+		// ResponseEntity<Coupon> coupon =
+		// restTemplate.getForEntity("http://COUPON-SERVICE/coupons/" +
+		// product.getCouponCode(), Coupon.class);
 		// ResponseEntity<Coupon>
 		// coupon=restTemplate.getForEntity("http://localhost:1234/coupons/"+product.getCouponCode(),
 		// Coupon.class, uriVariables);
-		Coupon couponObject = coupon.getBody();
-		product.setPrice(product.getPrice() - couponObject.getDiscount());
+		// Coupon couponObject = coupon.getBody();
+		Coupon coupon = restClient.getCoupon(product.getCouponCode());
+		product.setPrice(product.getPrice() - coupon.getDiscount());
 
 		return productService.insertProduct(product);
 
+	}
+
+	@GetMapping("/getCoupons")
+	public List<Coupon> viewCoupons() {
+		List<Coupon> coupons = restClient.getAllCoupons();
+		return coupons;
 	}
 
 }
