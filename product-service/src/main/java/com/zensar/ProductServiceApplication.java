12bd58a6e6cf3c9bc63593ecd04d13e493b9d37b
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-26 06:46:49 +0530

    Update ProductServiceApplication.java

--- product-service/src/main/java/com/zensar/ProductServiceApplication.java
+++ product-service/src/main/java/com/zensar/ProductServiceApplication.java
@@ -2,10 +2,15 @@ package com.zensar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
@@ -13,6 +18,7 @@ public class ProductServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 
